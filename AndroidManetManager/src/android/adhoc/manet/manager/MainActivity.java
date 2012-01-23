cc3d3b592044ae9100545dc800755af907496e80
jrobble <jrobble@mitre.org>
2012-01-23 11:40:00 -0500

    Simplified user-facing options.

--- AndroidManetManager/src/android/adhoc/manet/manager/MainActivity.java
+++ AndroidManetManager/src/android/adhoc/manet/manager/MainActivity.java
@@ -202,13 +202,13 @@ public class MainActivity extends Activity implements ManetObserver {
     	boolean supRetVal = super.onCreateOptionsMenu(menu);
     	SubMenu setup = menu.addSubMenu(0, MENU_CHANGE_SETTINGS, 0, getString(R.string.main_activity_settings));
     	setup.setIcon(drawable.ic_menu_preferences);
-    	SubMenu log = menu.addSubMenu(0, MENU_LOG, 0, getString(R.string.main_activity_showlog));
-    	log.setIcon(drawable.ic_menu_agenda);
+    	// SubMenu log = menu.addSubMenu(0, MENU_LOG, 0, getString(R.string.main_activity_showlog));
     	SubMenu about = menu.addSubMenu(0, MENU_ABOUT, 0, getString(R.string.main_activity_about));
     	about.setIcon(drawable.ic_menu_info_details);
     	// SubMenu console = menu.addSubMenu(0, MENU_CONSOLE, 0, getString(R.string.main_activity_console));
     	SubMenu send = menu.addSubMenu(0, MENU_SEND_MESSAGE, 0, getString(R.string.main_activity_send_message));
     	SubMenu info = menu.addSubMenu(0, MENU_GET_ROUTING_INFO, 0, getString(R.string.main_activity_get_routing_info));
+    	info.setIcon(drawable.ic_menu_agenda);
     	return supRetVal;
     }
     
