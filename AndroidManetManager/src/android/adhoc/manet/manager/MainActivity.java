e01a0ad44b88f661f09136b5b24826df9329b1eb
jrobble <jrobble@mitre.org>
2012-05-26 04:54:18 -0400

    Updated GPLv3 code comments.

--- AndroidManetManager/src/android/adhoc/manet/manager/MainActivity.java
+++ AndroidManetManager/src/android/adhoc/manet/manager/MainActivity.java
@@ -3,15 +3,21 @@
  *  Copyright (c) 2012 The MITRE Corporation.
  */
 /**
+ *  Portions of this code are copyright (c) 2009 Harald Mueller and Sofia Lemons.
+ * 
  *  This program is free software; you can redistribute it and/or modify it under 
  *  the terms of the GNU General Public License as published by the Free Software 
  *  Foundation; either version 3 of the License, or (at your option) any later 
  *  version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
  *  You should have received a copy of the GNU General Public License along with 
  *  this program; if not, see <http://www.gnu.org/licenses/>. 
  *  Use this application at your own risk.
- *  
- *  Portions of this code are copyright (c) 2009 Harald Mueller and Sofia Lemons.
  */
 package android.adhoc.manet.manager;
 
